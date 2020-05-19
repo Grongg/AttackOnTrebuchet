9f38ec2b6b5ffab60f7973fbb7f7cf5fa6adedf6
grong <lionel.de-sousa@epitech.eu>
2020-05-19 04:40:03 +0200

    [ADD] Grail's health

---
+++ Assets/Scripts/GrailControler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrailControler : MonoBehaviour
+{
+    [SerializeField] private GameObject[] hearts;
+    private int currentHearts;
+    void Start()
+    {
+        currentHearts = 3;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (currentHearts - 1 > 0)
+            {
+                Destroy(hearts[currentHearts - 1]);
+                currentHearts--;
+            }
+            else
+            {
+                Destroy(hearts[currentHearts - 1]);
+                //insert death scene loader 
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
